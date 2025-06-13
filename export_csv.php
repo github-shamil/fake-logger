0f462a027a89041d699f88e84bb1274446ba5ec1
github-shamil <168990260+github-shamil@users.noreply.github.com>
2025-06-13 19:53:34 +0530

    Update export_csv.php

--- export_csv.php
+++ export_csv.php
@@ -1,34 +1,14 @@
 <?php
-// Secure download of log data from SQLite to CSV
-
 $db = new SQLite3('log.db');
-
-// Set headers
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="visitor_logs.csv"');
-
-// Open output stream
 $output = fopen('php://output', 'w');
-
-// CSV headers
-fputcsv($output, ['IP Address', 'Latitude', 'Longitude', 'City', 'Country', 'Timestamp', 'Google Maps Link']);
-
-// Query DB
+fputcsv($output, ['IP', 'Latitude', 'Longitude', 'City', 'Country', 'Timestamp', 'Google Maps Link']);
 $results = $db->query('SELECT * FROM logs ORDER BY id DESC');
-
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-    $mapLink = "https://www.google.com/maps?q={$row['latitude']},{$row['longitude']}";
-    fputcsv($output, [
-        $row['ip'],
-        $row['latitude'],
-        $row['longitude'],
-        $row['city'],
-        $row['country'],
-        $row['timestamp'],
-        $mapLink
-    ]);
+  $link = "https://www.google.com/maps?q={$row['latitude']},{$row['longitude']}";
+  fputcsv($output, [$row['ip'], $row['latitude'], $row['longitude'], $row['city'], $row['country'], $row['timestamp'], $link]);
 }
-
 fclose($output);
 exit;
 ?>
