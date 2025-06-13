c85ca832fb9727f120b98ae321ee4be48a3bd6df
github-shamil <168990260+github-shamil@users.noreply.github.com>
2025-06-13 19:55:56 +0530

    Update download_db.php

--- download_db.php
+++ download_db.php
@@ -1,8 +1,5 @@
 <?php
-// Force download of raw SQLite database file
-
 $filename = 'log.db';
-
 if (file_exists($filename)) {
     header('Content-Type: application/octet-stream');
     header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
@@ -10,6 +7,6 @@ if (file_exists($filename)) {
     readfile($filename);
     exit;
 } else {
-    echo "Database file not found.";
+    echo "Database not found.";
 }
 ?>
