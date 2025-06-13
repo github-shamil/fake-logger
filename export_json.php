17de9caf9019a3d38839fa202e3ebe2ee8d57bd4
github-shamil <168990260+github-shamil@users.noreply.github.com>
2025-06-13 20:47:56 +0530

    Update export_json.php

--- export_json.php
+++ export_json.php
@@ -1,13 +1,17 @@
 <?php
 $db = new SQLite3('log.db');
+
 header('Content-Type: application/json');
 header('Content-Disposition: attachment; filename="visitor_logs.json"');
-$data = [];
+
 $results = $db->query('SELECT * FROM logs ORDER BY id DESC');
+$data = [];
+
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-  $row['google_maps_link'] = "https://www.google.com/maps?q={$row['latitude']},{$row['longitude']}";
-  $data[] = $row;
+    $row['google_maps_link'] = "https://www.google.com/maps?q={$row['latitude']},{$row['longitude']}";
+    $data[] = $row;
 }
+
 echo json_encode($data, JSON_PRETTY_PRINT);
 exit;
 ?>
