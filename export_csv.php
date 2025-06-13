3c058c44e8adcdfb9e7b67c30c246faacf4267b6
github-shamil <168990260+github-shamil@users.noreply.github.com>
2025-06-13 20:47:32 +0530

    Update export_csv.php

--- export_csv.php
+++ export_csv.php
@@ -1,14 +1,27 @@
 <?php
 $db = new SQLite3('log.db');
+
+// Headers
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="visitor_logs.csv"');
+
+// Output stream
 $output = fopen('php://output', 'w');
+
+// CSV Header
 fputcsv($output, ['IP', 'Latitude', 'Longitude', 'City', 'Country', 'Timestamp', 'Google Maps Link']);
+
+// Query
 $results = $db->query('SELECT * FROM logs ORDER BY id DESC');
+
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-  $link = "https://www.google.com/maps?q={$row['latitude']},{$row['longitude']}";
-  fputcsv($output, [$row['ip'], $row['latitude'], $row['longitude'], $row['city'], $row['country'], $row['timestamp'], $link]);
+    $mapLink = "https://www.google.com/maps?q={$row['latitude']},{$row['longitude']}";
+    fputcsv($output, [
+        $row['ip'], $row['latitude'], $row['longitude'],
+        $row['city'], $row['country'], $row['timestamp'], $mapLink
+    ]);
 }
+
 fclose($output);
 exit;
 ?>
